9c4890b256e635bc81ebc852abc193835d6556f5
LenChang_Personal <lamgo.chang@gmail.com>
2021-01-29 00:14:05 +0800

    Add inviter info

---
+++ src/static/contact.ts
@@ -0,0 +1,13 @@
+export const contact: {
+  userName: string;
+  userEmail: string;
+  peopleCount: number;
+  tableIndex: string;
+}[] = [
+  {
+    userName: "張懷倫",
+    userEmail: "lamgo.chang@gmail.com",
+    peopleCount: 1,
+    tableIndex: "1-a",
+  },
+];
