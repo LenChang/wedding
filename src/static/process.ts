deb356d0f7e2bfa1932ed142e7331caa3fac8450
LenChangForAMI <len.chang@aeonmatrix.com>
2021-02-03 19:23:45 +0800

    Add data : process

---
+++ src/static/process.ts
@@ -0,0 +1,71 @@
+export const witnessMarriageEvent: {
+  time: string;
+  content: string;
+  building: string;
+  googleMap: string;
+}[] = [
+  {
+    time: "AM 9:30 ~ AM 10:45",
+    content: "賓客入場",
+    building: "許諾禮堂",
+    googleMap: "https://goo.gl/maps/Htb7SkPnPGpoVHTx8",
+  },
+  {
+    time: "AM 09:45 ~ AM 10:05",
+    content: "證婚儀式",
+    building: "許諾禮堂",
+    googleMap: "https://goo.gl/maps/Htb7SkPnPGpoVHTx8",
+  },
+  {
+    time: "AM 10:10 ~ AM 10:30",
+    content: "大合照",
+    building: "許諾禮堂",
+    googleMap: "https://goo.gl/maps/Htb7SkPnPGpoVHTx8",
+  },
+];
+
+export const marriageEvent: {
+  time: string;
+  content: string;
+  building: string;
+  googleMap: string;
+}[] = [
+  {
+    time: "AM 11:30 ~ PM 12:00",
+    content: "賓客入場",
+    building: "歡沁A廳",
+    googleMap: "https://goo.gl/maps/JT1GUUpsDYHogAs4A",
+  },
+  {
+    time: "PM 12:10 ~ PM 3:00",
+    content: "午宴",
+    building: "歡沁A廳",
+    googleMap: "https://goo.gl/maps/JT1GUUpsDYHogAs4A",
+  },
+  {
+    time: "PM 3:15",
+    content: "接駁車出發(心之芳庭",
+    building: "未定",
+    googleMap: "未定",
+  },
+];
+
+export const afterParty: {
+  time: string;
+  content: string;
+  building: string;
+  googleMap: string;
+}[] = [
+  {
+    time: "PM 4:00 ~ PM 6:00",
+    content: "After Party",
+    building: "小步舞曲",
+    googleMap: "未定",
+  },
+  {
+    time: "PM 6:30",
+    content: "接駁車出發(心之芳庭)",
+    building: "未定",
+    googleMap: "未定",
+  },
+];
