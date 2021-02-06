32604321a8f1e13d432e9f711c0de4b5e574f994
Len Chang <lamgo.chang@gmail.com>
2021-02-06 19:14:46 +0800

    for testing

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,15 @@ import "./index.css";
 import App from "./App";
 import { ReactComponent as Svg } from "./logo.svg";
 import reportWebVitals from "./reportWebVitals";
+import MetaTags from "react-meta-tags";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MetaTags>
+      <title>Test1234</title>
+      <meta name="description" content="Some description for Test1234" />
+      <meta property="og:title" content="MyApp for Test1234" />
+    </MetaTags>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
